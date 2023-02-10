dae2bac26792f884efc0ed6f07e603b04cb11c9b
Mohamed Hakem <mohamedhakem959628@gmail.com>
2023-02-10 20:28:27 +0200

    updating chrome.js

--- functions/chrome/chrome.js
+++ functions/chrome/chrome.js
@@ -24,7 +24,7 @@ exports.handler = async (event, context) => {
       waitUntil: ["domcontentloaded", "networkidle0"]
     })
 
-    await page.waitForSelector('#phenomic')
+//     await page.waitForSelector('#phenomic')
 
     theTitle = await page.title();
 
